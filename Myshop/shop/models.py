4c73aa5f5ffd389c1e195e0b410a5722c9fc932c
engkamakia <georgekamakia@gmail.com>
2023-11-17 20:43:26 +0300

    import reverse in the models and add functions to get absolute url

--- Myshop/shop/models.py
+++ Myshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -8,14 +9,15 @@ class Category(models.Model):
     
     class Meta:
         ordering = ['name']
-        indexes = [
-        models.Index(fields=['name']),
-        ]
-    verbose_name = 'category'
-    verbose_name_plural = 'categories'
-    def __str__(self):
-        return self.name
-    
+        indexes = [models.Index(fields=['name']),]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        def __str__(self):
+            return self.name
+        
+        def get_absolute_url(self):
+            return reverse('shop:product_list_by_category',
+            args=[self.slug])
     
 class Product(models.Model):
     category = models.ForeignKey(Category,
@@ -32,13 +34,18 @@ class Product(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     
-class Meta:
-    ordering = ['name']
-    indexes = [
-    models.Index(fields=['id', 'slug']),
-    models.Index(fields=['name']),
-    models.Index(fields=['-created']),
-    ]
+    class Meta:
+        ordering = ['name']
+        indexes = [
+        models.Index(fields=['id', 'slug']),
+        models.Index(fields=['name']),
+        models.Index(fields=['-created']),
+        ]
+        
     
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+        args=[self.id, self.slug])
