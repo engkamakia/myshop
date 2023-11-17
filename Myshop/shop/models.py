48de19c97e930b8f34e20fae25b9b5023feea79e
engkamakia <georgekamakia@gmail.com>
2023-11-17 22:02:21 +0300

    in settings.py add the cart session's keys

--- Myshop/shop/models.py
+++ Myshop/shop/models.py
@@ -12,11 +12,11 @@ class Category(models.Model):
         indexes = [models.Index(fields=['name']),]
         verbose_name = 'category'
         verbose_name_plural = 'categories'
-        def __str__(self):
+    def __str__(self):
             return self.name
         
-        def get_absolute_url(self):
-            return reverse('shop:product_list_by_category',
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
             args=[self.slug])
     
 class Product(models.Model):
