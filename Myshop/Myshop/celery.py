a11f6c90cbbbcb8e637814d7612d6ed5d71425c0
engkamakia <georgekamakia@gmail.com>
2023-11-18 22:14:03 +0300

    create a tasks.py for easy task discovery by Celery

--- Myshop/Myshop/celery.py
+++ Myshop/Myshop/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
-app = Celery('myshop')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Myshop.settings')
+app = Celery('Myshop')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
