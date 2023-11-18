9b8d7e23f28a2ac89059382506ec42a826c3792d
engkamakia <georgekamakia@gmail.com>
2023-11-18 15:18:48 +0300

    create a cart context processor so as to display amount of items in cart at the navbar icon

--- Myshop/Myshop/settings.py
+++ Myshop/Myshop/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'cart.context_processors.cart',
 ]
 
 ROOT_URLCONF = 'Myshop.urls'
