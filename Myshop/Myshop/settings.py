5b44d53e1e134e860f9afa07334e55ac9e7e2ce9
engkamakia <georgekamakia@gmail.com>
2023-11-19 22:39:37 +0300

    enable order view to send email to user asynchronously

--- Myshop/Myshop/settings.py
+++ Myshop/Myshop/settings.py
@@ -84,6 +84,17 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'aboderealtorske2@gmail.com'
+EMAIL_HOST_PASSWORD = 'rgufylhcwyqcspsm'
+#DEFAULT_FROM_EMAIL = 'myusername@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#SERVER_EMAIL = 'myusername@gmail.com'
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
